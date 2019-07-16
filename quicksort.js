453a615e09d5da922892516be2ef15d54ed8c3c6
Raghavamohank <raghavamohank@gmail.com>
2019-07-16 14:11:49 +0530

    quicksort

---
+++ quicksort.js
@@ -0,0 +1,47 @@
+function swap(items, leftIndex, rightIndex) {
+    let temp = items[leftIndex];
+    items[leftIndex] = items[rightIndex];
+    items[rightIndex] = temp;
+}
+
+function partition(items, left, right) {
+    let pivot = items[Math.floor((right+left)/2)],
+        i     = left;
+        j     = right;
+    
+    while(i <= j){
+        while(items[i]<pivot) {
+            i++;
+        }
+
+        while(items[j]> pivot) {
+            j--;
+        }
+
+        if(i<=j) {
+            swap(items,i,j);
+            i++;
+            j--;
+        }
+    }
+
+    return i;
+}
+
+function quicksort(items,left,right) {
+    let index;
+    if(items.length>1) {
+        index = partition(items,left,right); 
+        if(left < index-1) {
+            quicksort(items,left, index-1);
+        }
+        if(index < right) {
+            quicksort(items,index,right);
+        }
+    }
+    return items;
+}
+
+var items = [5,3,7,6,2,9];
+var sortedArray = quicksort(items,0, items.length-1);
+console.log(sortedArray);
